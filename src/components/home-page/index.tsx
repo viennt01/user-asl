6f86f447a466ba14ca230bd0110851ad0a5dde55
viennt01 <thanhviennguyen01@gmail.com>
2023-11-25 16:40:57 +0700

    feat/ introduce home

--- src/components/home-page/index.tsx
+++ src/components/home-page/index.tsx
@@ -1,6 +1,5 @@
 import style from './index.module.scss';
 import Welcome from './components/welcome';
-import Information from './components/information';
 import Service from './components/service';
 import Introduce from './components/introduce';
 
@@ -8,7 +7,6 @@ export default function Home() {
   return (
     <div className={style.homePageContainer}>
       <Welcome />
-      <Information />
       <Introduce />
       <Service />
     </div>
